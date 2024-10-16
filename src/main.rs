a9d399e53922e53335292f7901a9f87bc66f89e3
lhamouche <luxel.hamouche.lh@gmail.com>
2024-10-16 16:36:11 +0200

    Handle key combinations

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,25 @@ struct InputEvent {
     value: c_uint,
 }
 
+fn c_uint_to_key_direction(num: c_uint) -> Option<xkb::KeyDirection> {
+    match num {
+        0 => Some(xkb::KeyDirection::Up),
+        1 => Some(xkb::KeyDirection::Down),
+        _ => None,
+    }
+}
+
 fn read_input_event(file: &mut File, buffer: &mut [u8]) -> io::Result<InputEvent> {
     file.read_exact(buffer)?;
     Ok(unsafe { std::ptr::read(buffer.as_ptr() as *const _) })
 }
 
-fn handle_event(input_event: &InputEvent, state: &xkb::State) {
+fn handle_event(input_event: &InputEvent, state: &mut xkb::State) {
     if input_event.type_ == 0x01 {
         let key_code = xkb::Keycode::new(input_event.code as u32 + 8);
+        if let Some(key_direction) = c_uint_to_key_direction(input_event.value) {
+            state.update_key(key_code, key_direction);
+        }
         let utf8 = state.key_get_utf8(key_code);
         println!("{:?} {:?}", input_event, utf8);
     }
@@ -48,11 +59,11 @@ fn main() -> io::Result<()> {
         xkb::KEYMAP_COMPILE_NO_FLAGS,
     )
     .expect("[!] Failed to create keymap");
-    let state = xkb::State::new(&keymap);
+    let mut state = xkb::State::new(&keymap);
 
     loop {
         match read_input_event(&mut file, &mut buffer) {
-            Ok(input_event) => handle_event(&input_event, &state),
+            Ok(input_event) => handle_event(&input_event, &mut state),
             Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => break,
             Err(e) => {
                 eprintln!("[!] Error while reading: {:?}", e);
